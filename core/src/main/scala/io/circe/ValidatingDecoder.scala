665fb17e6e07cdf746eefe8595cc3413b7a0ea35
Niklas Klein <mail@taig.io>
2020-07-22 11:07:16 +0200

    Add ValidatingDecoder.apply and .lift as a preferred way to instantiate ValidatingDecoder

--- core/src/main/scala/io/circe/ValidatingDecoder.scala
+++ core/src/main/scala/io/circe/ValidatingDecoder.scala
@@ -1,26 +1,38 @@
 package io.circe
 
+import cats.data.{Validated, ValidatedNel}
 import cats.syntax.either._
 
 trait ValidatingDecoder[A] extends Decoder[A] {
-  override def apply(cursor: HCursor): Decoder.Result[A] =
-    decodeValidating(cursor).toEither.leftMap(_.head)
+  override def apply(cursor: HCursor): Decoder.Result[A] = decodeValidating(cursor).toEither.leftMap(_.head)
 
-  override def decodeAccumulating(
-      cursor: HCursor
-  ): Decoder.AccumulatingResult[A] =
-    decodeValidating(cursor)
+  override def decodeAccumulating(cursor: HCursor): Decoder.AccumulatingResult[A] = decodeValidating(cursor)
 
   def decodeValidating(cursor: HCursor): Decoder.AccumulatingResult[A]
 }
 
 object ValidatingDecoder {
-  def instance[A](
-      f: HCursor => Decoder.AccumulatingResult[A]
-  ): ValidatingDecoder[A] =
+  def instance[A](f: HCursor => Decoder.AccumulatingResult[A]): ValidatingDecoder[A] =
     new ValidatingDecoder[A] {
-      override def decodeValidating(
-          cursor: HCursor
-      ): Decoder.AccumulatingResult[A] = f(cursor)
+      override def decodeValidating(cursor: HCursor): Decoder.AccumulatingResult[A] = f(cursor)
     }
+
+  def lift[A, B](decoder: Decoder[A])(validate: A => ValidatedNel[String, B]): ValidatingDecoder[B] =
+    ValidatingDecoder.instance { cursor =>
+      Validated
+        .fromEither(decoder(cursor))
+        .toValidatedNel
+        .andThen(liftDecodingFailures[A, B](validate, cursor))
+    }
+
+  private def liftDecodingFailures[A, B](
+      validate: A => ValidatedNel[String, B],
+      cursor: HCursor
+  ): A => ValidatedNel[DecodingFailure, B] = validate.andThen(_.leftMap(_.map(DecodingFailure(_, cursor.history))))
+
+  def apply[A](implicit decoder: Decoder[A]): ApplyBuilder[A] = new ApplyBuilder[A](decoder)
+
+  final class ApplyBuilder[A](val decoder: Decoder[A]) extends AnyVal {
+    def apply[B](validate: A => ValidatedNel[String, B]): ValidatingDecoder[B] = lift(decoder)(validate)
+  }
 }
